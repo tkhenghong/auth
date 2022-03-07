8e6f0e3768161e4efb6077420da208542cfed705
Teoh Kheng Hong <tkhenghong@gmail.com>
2022-03-08 00:30:54 +0800

    first commit

---
+++ src/test/java/com/awpghost/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awpghost.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
